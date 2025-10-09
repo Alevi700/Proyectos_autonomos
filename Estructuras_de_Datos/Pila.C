0fbd4700c6148484188b3c57afd2c557abc3d06d
Alevi700 <Alejandro.Vicioso1@alu.uclm.es>
2025-10-09 16:45:24 +0200

    Pila en C

---
+++ Estructuras_de_Datos/Pila.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+
+struct Nodo{
+    Nodo* next;
+    void* element; 
+};
+struct Stack{
+    Nodo peek;
+    int size;
+};
+
+void push(void* element){
+    
+}
+
+int main(){
+
+
+}
