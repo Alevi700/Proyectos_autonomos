69ec7b3e21ea67fb5c2402e1c49b40164767199a
Vicioso <Alejandro.Vicioso1@alu.uclm.es>
2026-03-16 19:50:04 +0100

    Base para lanzar comandos

--- Varios/minishel/minishell.c
+++ Varios/minishel/minishell.c
@@ -10,13 +10,25 @@ typedef struct{
     int argc;
 }Command;
 
+typedef struct{
+    char* name;
+    void (*fun)(Command*);
+
+}Builtin;
+
 void read_comand(char* buffer);
 void parse_comand(char* buffer, Command* cmmd);
-
-
+void built_in_exit(Command* cmmd);
+int is_builtin(Command* cmmd, Builtin* bltin);
 
 int main() {
     
+    Builtin bltin[] = 
+    {
+        {"exit", built_in_exit},
+        {NULL, NULL}
+    };
+
     Command* cmmd = malloc(sizeof(Command));
     cmmd->command = NULL;
     cmmd->argv = malloc(sizeof(char*)*MAX_ARG);
@@ -26,7 +38,11 @@ int main() {
     while (1){
 
         read_comand(buffer);
-        parse_comand(buffer, cmmd);        
+        parse_comand(buffer, cmmd);
+
+        if(!is_builtin(cmmd, bltin)){
+            printf("Not builtin\n");
+        }
     }
 
 }
@@ -38,7 +54,7 @@ void read_comand(char* buffer){
     do{
         printf(">");
         fgets(line, sizeof(line), stdin);
-        if(strcmp(line,"\n") == 0) line[0] = '\0';
+        line[strcspn(line, "\n")] = '\0';
 
     }while(line[0] == '\0');
     
@@ -61,7 +77,27 @@ void parse_comand(char* buffer, Command* cmmd){
 
     cmmd->argv[i] = NULL;
     cmmd->argc = i;
-    for(int j = 0; j<i; j++){
-        printf("%s ", cmmd->argv[j]);
+}
+
+void built_in_exit(Command* cmmd){
+
+    printf("Quiting...\n");
+
+    exit(1);
+}
+
+int is_builtin(Command* cmmd, Builtin* bltin){
+
+    int i = 0;
+
+    while(bltin[i].name != NULL){
+        if(strcmp(bltin[i].name, cmmd->command) == 0){
+            bltin[i].fun(cmmd);
+            return 0;
+        }
+        i++;
     }
+    return -1;
+
+    
 }
